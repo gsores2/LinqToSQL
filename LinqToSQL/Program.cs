5deb3a71f229d816e6431b2c5826549c681dafcf
Giulia Soresini <giulia.soresini@gmail.com>
2020-11-26 12:44:59 +0100

    Add project files.

---
+++ LinqToSQL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = @"Persist Security Info=false; Integrated Security=true; Initial Catalog = CinemaDb; Server = WINAP4R3GZJOYFF\SQLEXPRESS";
+
+        }
+    }
+}
